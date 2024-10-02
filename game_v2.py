961c1323abf54373862418762046e66ad7cdbda9
hishnick5 <100071396+hishnick5@users.noreply.github.com>
2024-10-02 22:06:35 +0300

    Update_2.10.2024
    Update_2.10.2024

---
+++ game_v2.py
@@ -0,0 +1,63 @@
+'''
+Сгенерируем документацию для нашей функции. Для этого в теле функции прописываем тройные кавычки (символ docstring),
+и, если у вас установлено расширение Python Docstring Generator, шаблон документации будет создан автоматически. 
+На месте [summary] прописываем описание функции, а вместо [description] указываем описание аргументов и возвращаемого значения:
+'''
+import numpy as np
+
+def random_predict(number:int=1) -> int:
+    """Рандомно угадываем число
+
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+
+    count = 0
+
+    while True:
+        count += 1
+        predict_number = np.random.randint(1, 101) # предполагаемое число
+        if number == predict_number:
+            break # выход из цикла, если угадали
+    return(count)
+
+# print(f'Количество попыток: {random_predict()}')
+
+
+'''
+ВНОВЬ МОДИФИЦИРУЕМ ЗАДАЧУ
+
+В том же файле game_v2.py напишем функцию score_game(), определяющую среднее количество попыток угадывания числа. Аргументом функции будет другая функция, с помощью которой и реализуется угадывание числа. 
+
+Пропишем для этой функции документацию. Алгоритм работы функции вы можете прочитать в комментариях:
+'''
+
+def score_game(random_predict) -> int:
+    """За какое количество попыток в среднем из 1000 подходов угадывает наш алгоритм
+
+    Args:
+        random_predict ([type]): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
+
+    count_ls = [] # список для сохранения количества попыток
+    np.random.seed(1) # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(1000)) # загадали список чисел
+
+    for number in random_array:
+        count_ls.append(random_predict(number))
+
+    score = int(np.mean(count_ls)) # находим среднее количество попыток
+
+    print(f'Ваш алгоритм угадывает число в среднем за: {score} попыток')
+    return(score)
+
+# RUN
+# score_game(random_predict)
+if __name__ == '__main__':
+    score_game(random_predict)
