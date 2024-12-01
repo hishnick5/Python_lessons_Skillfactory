c567d54fd10ed1b1a5710a591b916f69308b7c6a
hishnick5 <hishnick5@mail.ru>
2024-12-01 16:28:31 +0300

    16:28 _01_12_2024

--- game_v2.py
+++ game_v2.py
@@ -57,7 +57,5 @@ def score_game(random_predict) -> int:
     print(f'Ваш алгоритм угадывает число в среднем за: {score} попыток')
     return(score)
 
-# RUN
-# score_game(random_predict)
 if __name__ == '__main__':
     score_game(random_predict)
